d54bcbb6bbff33a913d0404e0544810febddc87c
Bonieky Lacerda - B7Web <suporte@b7web.com.br>
2021-07-05 16:52:14 -0300

    adding files to repo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# node-ts-mod1-mvc
+Arquivos do módulo inicial de Node + Typescript, da estrutura MVC aplicada.
+
+### Pré-requisitos globais:
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+import mustache from 'mustache-express';
+import mainRoutes from './routes/index';
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+server.use(express.urlencoded({extended: true}));
+
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada!');
+});
+
+server.listen(80);
